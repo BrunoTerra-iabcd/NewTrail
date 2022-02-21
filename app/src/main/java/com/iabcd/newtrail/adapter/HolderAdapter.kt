4cf3d78d6c61d0f44826b10a60760f21fda2fbf5
BrunoTerra-iabcd <bterra@institutoabcd.org.br>
2022-02-21 16:14:45 -0300

    Added props and svgs to xml conception.

--- app/src/main/java/com/iabcd/newtrail/adapter/HolderAdapter.kt
+++ app/src/main/java/com/iabcd/newtrail/adapter/HolderAdapter.kt
@@ -4,11 +4,13 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.iabcd.newtrail.databinding.RowHolderBinding
 import com.iabcd.newtrail.model.Holder
+import kotlin.random.Random
 
-class HolderAdapter(private val items: List<Holder>, private val onClick : (View,Holder) -> Unit) :
+class HolderAdapter(private val items: List<Holder>, private val onClick: (View, Holder) -> Unit) :
     RecyclerView.Adapter<HolderAdapter.HolderViewHolder>() {
 
     inner class HolderViewHolder(private val mBinder: RowHolderBinding) :
@@ -19,21 +21,45 @@ class HolderAdapter(private val items: List<Holder>, private val onClick : (View
             mBinder.rowHolderTxtName.text = holder.name
             setParentGravity(position)
             setCLickListener(holder)
-
+            handleProps(position)
         }
 
         private fun setParentGravity(position: Int) {
             if (position % 2 == 0) {
-                mBinder.root.gravity = Gravity.START
+                mBinder.rowHolderContainer.gravity = Gravity.START
             } else {
-                mBinder.root.gravity = Gravity.END
+                mBinder.rowHolderContainer.gravity = Gravity.END
             }
         }
 
-        private fun setCLickListener(holder: Holder){
+        private fun setCLickListener(holder: Holder) {
             mBinder.rowHolderImage.setOnClickListener {
-                onClick(it,holder)
+                onClick(it, holder)
             }
+        }
+
+        private fun handleProps(position: Int) {
+
+            val random = Random.nextInt(0, 3)
+
+            if (random == 0 || random == 1) {
+                mBinder.rowHolderProp1.isVisible = true
+                mBinder.rowHolderProp1.x += Random.nextInt(-200, 200)
+            } else {
+                mBinder.rowHolderProp1.isVisible = false
+            }
+
+            if (position > items.size / 3) {
+                mBinder.rowHolderProp1.isVisible = false
+
+                mBinder.rowHolderProp2.isVisible = random == 1
+                mBinder.rowHolderProp3.isVisible = random == 2
+
+            }else{
+                mBinder.rowHolderProp2.isVisible = false
+                mBinder.rowHolderProp3.isVisible = false
+            }
+
         }
 
     }
