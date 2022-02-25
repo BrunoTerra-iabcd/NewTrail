c8d6d1c7c970ae8cbdddc14bc35d484401fd185a
BrunoTerra-iabcd <bterra@institutoabcd.org.br>
2022-02-25 12:11:13 -0300

    MotionHolderAdapter.kt now has only one ViewHolder.

--- app/src/main/java/com/iabcd/newtrail/adapter/MotionHolderAdapter.kt
+++ app/src/main/java/com/iabcd/newtrail/adapter/MotionHolderAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.google.android.material.transition.Hold
 import com.iabcd.newtrail.databinding.RowHolderMotionLeftBinding
 import com.iabcd.newtrail.databinding.RowHolderMotionRightBinding
@@ -48,18 +49,46 @@ class MotionHolderAdapter(
         }
     }
 
+    inner class MotionViewHolder (private val mBinder : ViewBinding) : RecyclerView.ViewHolder(mBinder.root){
+
+        fun bind(holder: Holder,position: Int){
+
+            when(mBinder){
+
+                is RowHolderMotionLeftBinding ->{
+                    mBinder.textView2.text = holder.name
+
+                    mBinder.imageView3.setOnClickListener {
+                        onClick(it,holder,position,mBinder.root)
+                    }
+                }
+
+                is RowHolderMotionRightBinding ->{
+                    mBinder.textView2.text = holder.name
+
+                    mBinder.imageView3.setOnClickListener {
+                        onClick(it,holder,position,mBinder.root)
+                    }
+                }
+
+                else -> throw Exception("Invalid row")
+            }
+        }
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
 
         return when (viewType) {
 
-            TYPE_LEFT -> MotionHolderLeftViewHolder(
+            TYPE_LEFT -> MotionViewHolder(
                 RowHolderMotionLeftBinding.inflate(
                     LayoutInflater.from(parent.context), parent, false
                 )
             )
 
-            else -> MotionHolderRightViewHolder(
+            else -> MotionViewHolder(
                 RowHolderMotionRightBinding.inflate(
                     LayoutInflater.from(parent.context), parent, false
                 )
@@ -71,16 +100,7 @@ class MotionHolderAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
-        when(holder){
-
-            is MotionHolderLeftViewHolder -> {
-                holder.bind(items[position], position)
-            }
-
-            is MotionHolderRightViewHolder -> {
-                holder.bind(items[position], position)
-            }
-        }
+        (holder as MotionViewHolder).bind(items[position], position)
     }
 
     override fun getItemCount(): Int = items.size
